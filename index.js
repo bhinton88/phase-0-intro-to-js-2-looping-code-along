22f7ca0f158688fff9c5ea56287b19f13c7f203a
Bobby Hinton <bob.g.hint@gmail.com>
2022-04-16 18:38:46 -0500

    commit tree 9b4e672633c7cd733c5ee6005ac9bfe8b8d13b82

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+
+const messsages = [];
+
+function writeCards(names,event) {
+   
+    for (let i = 0; i < names.length; i++){
+
+        messsages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    // remember that return needs to remain outside the for loop in order for the loop to return all messages
+    return messsages;
+
+}
+
+function countDown(n) {
+    let i = 10
+    while (i >= 0 ) {
+        console.log(n);
+        n--;
+        i--;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
